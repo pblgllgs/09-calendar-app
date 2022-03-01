6e5169a43f537051001d6620107cab19f8d88579
pblgllgs <pbl.gllgs@gmail.com>
2022-03-01 10:17:25 -0300

    add big calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { Navbar } from '../ui/Navbar';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const myEventsList = [
+    {
+        title: 'Cloud Practitioner exam',
+        start: moment().toDate(),
+        end: moment().add(2, 'hours').toDate(),
+        bgcolor: '3966FF',
+    },
+];
 
 export const CalendarScreen = () => {
     return (
-        <div>
+        <div className='calendar-screen'>
             <Navbar />
+
+            <Calendar
+                localizer={localizer}
+                events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+            />
         </div>
     );
 };
