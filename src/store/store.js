5659266c151ed1c47cd49c9f6565493b9e7d3edf
pblgllgs <pbl.gllgs@gmail.com>
2022-03-03 11:12:55 -0300

    add store

---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { rootReducers } from '../reducers/rootReducer';
+
+
+const composeEnhancers =
+    (typeof window !== 'undefined' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
+
+
+
+export const store = createStore(
+    rootReducers,
+    composeEnhancers(applyMiddleware(thunk))
+);
