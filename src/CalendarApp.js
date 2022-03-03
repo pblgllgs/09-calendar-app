8329d023d63db044e4e80cc450f23d28514364e7
pblgllgs <pbl.gllgs@gmail.com>
2022-03-03 11:15:02 -0300

    add provider and store

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 import { AppRouter } from './router/AppRouter';
 
 export const CalendarApp = () => {
     return (
-        <>
+        <Provider store={store}>
             <AppRouter />
-        </>
+        </Provider>
     );
 };
