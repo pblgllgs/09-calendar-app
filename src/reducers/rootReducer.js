d7a7d582d0b466c322476db31d05c567c643d823
pblgllgs <pbl.gllgs@gmail.com>
2022-03-03 12:41:42 -0300

    add calendar reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
+import { calendarReducer } from "./calendarReducer";
 import { uiReducer } from "./uiReducer";
 
 
 
 export const rootReducer = combineReducers({
     ui: uiReducer,
-    //TODO: calendar
+    calendar: calendarReducer
     //TODO: auth
 });
