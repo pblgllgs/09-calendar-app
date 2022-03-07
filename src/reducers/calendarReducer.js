dfbab430b9b34e5a8645cf38af1bf1795f892a29
pblgllgs <pbl.gllgs@gmail.com>
2022-03-07 10:48:33 -0300

    add event updated

--- src/reducers/calendarReducer.js
+++ src/reducers/calendarReducer.js
@@ -1,26 +1,25 @@
-import moment from "moment";
-import { types } from "../types/types";
-
+import moment from 'moment';
+import { types } from '../types/types';
 
 const initialState = {
-    events:[{
-        id: new Date().getTime(),
-        title: 'Cloud Practitioner exam',
-        start: moment().toDate(),
-        end: moment().add(2, 'hours').toDate(),
-        bgcolor: '3966FF',
-        notes: 'Sacar hora',
-        user: {
-            _id: '123',
-            name: 'Pablo',
-        }
-    }],
-    activeEvent: null
+    events: [
+        {
+            id: new Date().getTime(),
+            title: 'Cloud Practitioner exam',
+            start: moment().toDate(),
+            end: moment().add(2, 'hours').toDate(),
+            bgcolor: '3966FF',
+            notes: 'Sacar hora',
+            user: {
+                _id: '123',
+                name: 'Pablo',
+            },
+        },
+    ],
+    activeEvent: null,
 };
 
-
-
-export const calendarReducer =  (state = initialState, action)=> {
+export const calendarReducer = (state = initialState, action) => {
     switch (action.type) {
         case types.eventSetActive:
             return {
@@ -30,14 +29,21 @@ export const calendarReducer =  (state = initialState, action)=> {
         case types.eventAddNew:
             return {
                 ...state,
-                events: [...state.events, action.payload]
+                events: [...state.events, action.payload],
             };
         case types.eventClearActiveEvent:
             return {
                 ...state,
                 activeEvent: null,
             };
+        case types.eventUpdated:
+            return {
+                ...state,
+                events: state.events.map((e) =>
+                    e.id === action.payload.id ? action.payload : e
+                ),
+            };
         default:
             return state;
     }
-}
+};
