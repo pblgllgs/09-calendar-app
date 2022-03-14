26cbde2326ac738ee7b566aa5cd79b7caa0860a9
pblgllgs <pbl.gllgs@gmail.com>
2022-03-14 15:43:50 -0300

    add fetchcontoken

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -16,4 +16,27 @@ const fetchSinToken = (endpoint, data, method = 'GET') => {
     }
 };
 
-export { fetchSinToken };
+const fetchConToken = (endpoint, data, method = 'GET') => {
+    const token = localStorage.getItem('token') || '';
+    const url = `${baseUrl}/${endpoint}`;
+
+    if (method === 'GET') {
+        return fetch(url, {
+            method,
+            headers: {
+                'x-token': token,
+            },
+        });
+    } else {
+        return fetch(url, {
+            method,
+            headers: {
+                'Content-type': 'application/json',
+                'x-token': token,
+            },
+            body: JSON.stringify(data),
+        });
+    }
+};
+
+export { fetchSinToken, fetchConToken };
