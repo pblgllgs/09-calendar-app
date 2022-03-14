c204f59b823924c63863e419663f197275473034
pblgllgs <pbl.gllgs@gmail.com>
2022-03-14 09:45:13 -0300

    add auth types

--- src/types/types.js
+++ src/types/types.js
@@ -1,9 +1,18 @@
 export const types = {
     uiOpenModal: '[ui] Open Modal',
     uiCloseModal: '[ui] Close Modal',
+
     eventSetActive: '[event] Set Active',
     eventAddNew: '[event] Add New',
     eventClearActiveEvent: '[event] Clear Active Event',
-    eventUpdated:  '[event] Update event',
-    eventDeleted: '[event] Deleted event'
+    eventUpdated: '[event] Update event',
+    eventDeleted: '[event] Deleted event',
+
+    authChecking: '[auth] Checking login state',
+    authCheckingFinish: '[auth] Finish Checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authLoginRegister: '[auth] Start Register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
 };
