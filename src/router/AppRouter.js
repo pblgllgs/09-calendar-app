2743d68b55edffbdadb7eacf56edc963b014b7c9
pblgllgs <pbl.gllgs@gmail.com>
2022-03-14 15:42:47 -0300

    check startChecking

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { startChecking } from '../actions/auth';
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
 
 export const AppRouter = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(startChecking());
+    }, [dispatch]);
+
     return (
         <Router>
             <Switch>
