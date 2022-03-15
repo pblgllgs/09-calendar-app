0dcf963f9f624fe78a118aea604f989c6809aec0
pblgllgs <pbl.gllgs@gmail.com>
2022-03-15 09:05:38 -0300

    implements handle logout

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { startLogout } from '../../actions/auth';
+import { eventClearAll } from '../../actions/events';
 
 export const Navbar = () => {
+    const { name } = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(startLogout());
+        dispatch(eventClearAll());
+    };
+
     return (
         <div className="navbar navbar-dark bg-dark mb-4">
-            <span className="navbar-brand">
-                React
-            </span>
-            <button className='btn btn-outline-danger'>
-                <i className='fas fa-sign-out-alt'></i>
+            <span className="navbar-brand">{name}</span>
+            <button onClick={handleLogout} className="btn btn-outline-danger">
+                <i className="fas fa-sign-out-alt"></i>
                 <span> Salir</span>
             </button>
         </div>
