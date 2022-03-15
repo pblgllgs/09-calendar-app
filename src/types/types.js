418723fe2a331be1f225e8cc62f3c8e458ea5b47
pblgllgs <pbl.gllgs@gmail.com>
2022-03-15 09:03:35 -0300

    add types load, clear, start new

--- src/types/types.js
+++ src/types/types.js
@@ -2,11 +2,14 @@ export const types = {
     uiOpenModal: '[ui] Open Modal',
     uiCloseModal: '[ui] Close Modal',
 
+    eventClearAll: '[event] Clear all',
     eventSetActive: '[event] Set Active',
+    eventStartAddNew: '[event] Start Set new',
     eventAddNew: '[event] Add New',
     eventClearActiveEvent: '[event] Clear Active Event',
     eventUpdated: '[event] Update event',
     eventDeleted: '[event] Deleted event',
+    eventLoaded: '[event] Event loaded',
 
     authChecking: '[auth] Checking login state',
     authCheckingFinish: '[auth] Finish Checking login state',
