d5bee00e1c4c04ab060447202f4b0ada046bc8b7
pblgllgs <pbl.gllgs@gmail.com>
2022-03-15 09:04:10 -0300

    private and public routes, replace routing

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,26 +1,40 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { startChecking } from '../actions/auth';
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import { Loading } from '../components/loading/Loading';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
     const dispatch = useDispatch();
+    const { checking, uid } = useSelector((state) => state.auth);
     useEffect(() => {
         dispatch(startChecking());
     }, [dispatch]);
 
+    if (checking) {
+        return <Loading />;
+    }
+
     return (
         <Router>
             <Switch>
-                <Route path="/login">
-                    <LoginScreen />
-                </Route>
-                <Route exact path="/">
-                    <CalendarScreen />
-                </Route>
+                <PublicRoute
+                    exact
+                    path="/login"
+                    component={LoginScreen}
+                    isAuthenticated={!!uid}
+                />
+                <PrivateRoute
+                    exact
+                    path="/"
+                    component={CalendarScreen}
+                    isAuthenticated={!!uid}
+                />
                 <Redirect to="/" />
             </Switch>
         </Router>
